2bf37e61789ee183c437fb6e02f3b17e4199c329
Michael Floren <flor3652@bears.unco.edu>
2015-02-22 23:32:38 -0700

    Setting the total as a global assignment, and creating an eventual instance to design a dataset when clicking the button (each click will add the "data point")

--- server.R
+++ server.R
@@ -1,10 +1,19 @@
 library(shiny)
 
+classData <- numeric()
+total <- 125
+
 shinyServer(function(input, output){
   
   output$score <- renderPrint({
-    total <- 125
     cat(total-input$numMiss, "/", total, " = ", (total-input$numMiss)/total, sep="")
   })
+  
+  output$data <- renderPrint({
+    input$submitButton
+    isolate(cat(total-input$numMiss, "/", total, " = ", (total-input$numMiss)/total, sep=""))
+  })
+  
+  
   
 })
